411cc33a3fa9c5e6d254f6cf6cc74ae76306610f
Mohamed <abdoashraf973@gmial.com>
2023-08-10 02:56:04 -0700

    Task Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
-int main(){
-        /*
-	 * description for function main?
-	 */
+int main(void){
 	putchar('"');
-        puts("Programming is like building a multilingual puzzle");
-        return 0;
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
