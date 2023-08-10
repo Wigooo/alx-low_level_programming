627f25ded81a1a25f045054c16461f1588d53471
Mohamed <abdoashraf973@gmial.com>
2023-08-10 03:02:53 -0700

    Task Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-/*
+/**
  * main - Entry point
  *
- * Description: A C Programe that prints with puts function
+ * Description: A C Programe that prints iwith put function
  *
  * Return: Always 0 (Success)
 */
