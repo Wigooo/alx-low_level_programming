d86841d83ccb2d1786a6e9e8af02f617dfed34a5
Mohamed <abdoashraf973@gmial.com>
2023-08-11 16:33:54 -0700

    Task Done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,8 @@
 /**
  * main - Entry point
  *
- * Description: A C program that prints the alphabet in lowercase, then in uppercase.
+ * Description: A C program that prints the alphabet in lowercase,
+ *              then in uppercase.
  *
  * Return: Always 0 (success)
 */
