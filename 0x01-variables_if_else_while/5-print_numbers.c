bc750958db426e9753be9c252aa05fb0ca6f72bb
Mohamed <abdoashraf973@gmial.com>
2023-08-11 16:49:30 -0700

    Task Done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that types numbers from 0 to 9
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
