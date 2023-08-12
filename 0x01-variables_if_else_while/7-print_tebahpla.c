22ff99b30e7e302b202bffe0e8f612b43caadba8
Mohamed <abdoashraf973@gmial.com>
2023-08-11 17:51:15 -0700

    Task Done

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A c program that print z to a
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char x = 'z';
+
+	while (x => 'a')
+	{
+		putchar(x);
+		x--;
+	}
+	putchar('\n'');
+
+	return (0);
+}
