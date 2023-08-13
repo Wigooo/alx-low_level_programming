a83a01dd9b5002966a94d0b4203b4cba5fb2e4bc
Mohamed <abdoashraf973@gmial.com>
2023-08-13 06:32:28 -0700

    Task Done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -10,4 +10,7 @@
 
 int main(void)
 {
-	print
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	return (1);
+}
