c878d74783e4dbb728c454792e05cdf4c4913229
Mohamed <abdoashraf973@gmial.com>
2023-08-15 15:04:08 -0700

    Task Done

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: A c program that prints using putchar
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int x;
+
+	for (x = 0; x < 8; x++)
+		_putchar(str[x]);
+	_putchar('\n');
+
+	return (0);
+}
