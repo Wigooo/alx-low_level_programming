9b0b57d2653a2901a86c449d064cc33f15411e70
Mohamed <abdoashraf973@gmial.com>
2023-08-15 15:58:30 -0700

    Task Done

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every minute of day
+*/
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar((x / 10) + 48);
+			_putchar((x % 10) + 48);
+			_putchar(':');
+			_putchar((y / 10) + 48);
+			_putchar((y % 10) + 48);
+			_putchar('\n');
+		}
+	}
+}
