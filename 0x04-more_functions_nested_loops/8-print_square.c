dbd7bcb8002dedea1347e84f347e909515098a2f
Mohamed <abdoashraf973@gmial.com>
2023-08-17 04:04:11 -0700

    Task Done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ *
+ * @size: size of the square
+*/
+
+void print_square(int size)
+{
+	int x, y;
+
+	for (x = 1; x <= size; x++)
+	{
+		for (y = 1; y < x; y++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
