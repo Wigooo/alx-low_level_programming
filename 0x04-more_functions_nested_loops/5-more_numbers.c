ac0fa117540693824b93aae89d463150e494bef8
Mohamed <abdoashraf973@gmial.com>
2023-08-17 02:28:52 -0700

    Task Done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - print 0 to 14 10 times
+*/
+
+void more_numbers(void)
+{
+	int x, y;
+
+	for (x = 0; x < 11; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			_putchar(y + 48);
+		}
+	}
+	_putchar('\n');
+}
