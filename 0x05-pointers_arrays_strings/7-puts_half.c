9d8f44a3d91f904c193794123128eeb7e6ef0351
Mohamed <abdoashraf973@gmial.com>
2023-08-21 18:02:54 -0700

    Task Done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "mainh.h"
+
+/**
+ * puts_half - print half of string with new line
+ *
+ * @str: input
+*/
+
+void puts_half(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+		;
+
+	x++;
+	for (x = x / 2; str[x] != '\0'; x++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
