b7e412338831b89c032f5610c44a48fa14e47636
Mohamed <abdoashraf973@gmial.com>
2023-08-21 17:53:21 -0700

    Task Done

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ *
+ * @s: string
+*/
+
+void rev_string(char *s)
+{
+	int x, y;
+	char z;
+
+	for (x = 0; s[x] != '\0'; x++)
+		;
+
+	for (y = 0; y < 1 / 2; y++)
+	{
+		z = s[y];
+		s[y] = s[x - 1 - y];
+		s[x - 1 - y] = z;
+	}
+}
