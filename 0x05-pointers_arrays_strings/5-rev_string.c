dc503e43976c2f2b0edb8ce117eecf7d4b0e1a7d
Mohamed <abdoashraf973@gmial.com>
2023-08-21 17:55:33 -0700

    Task Done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,10 @@ void rev_string(char *s)
 	int x, y;
 	char z;
 
-	for (x = 0; s[x] != '\0'; ++x)
-		;
+	for (x = 0; s[x] != '\0'; x++)
+		
 
-	for (y = 0; y < 1 / 2; y++)
+	for (y = 0; y < x / 2; y++)
 	{
 		z = s[y];
 		s[y] = s[x - 1 - y];
