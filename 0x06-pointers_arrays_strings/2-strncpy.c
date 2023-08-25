624bf950e15906ab5fb86d2e5e36a8fd68dd620c
Mohamed <abdoashraf973@gmial.com>
2023-08-24 17:11:22 -0700

    Task Done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,11 +18,10 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (x = 0; x < n && src[x] != '\0'; x++)
 		dest[x] = src[x];
-
 	while (x < n)
 	{
 		dest[x] = '\0';
-		x++
+		x++;
 	}
 
 	return (dest);
