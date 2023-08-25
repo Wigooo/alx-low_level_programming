3671af920bcb001b9089e5f9d92065a52493840b
Mohamed <abdoashraf973@gmial.com>
2023-08-24 17:43:27 -0700

    Task Done

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *str)
 
 	for (x = 0; str[x] != '\0'; x++)
 	{
-		if (str[x] > 97 && str[x] <= 122)
+		if (str[x] >= 97 && str[x] <= 122)
 		{
 			str[x] = str[x] - 32;
 		}
