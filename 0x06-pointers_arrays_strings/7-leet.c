80ba9edfa0006fb83eca8a6b954fcae0d213744f
Mohamed <abdoashraf973@gmial.com>
2023-08-24 18:12:40 -0700

    Task Done

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * *leet - encodes a string into 1337
+ *
+ * @x: pointer
+ *
+ * Return: 1337
+*/
+
+char *leet(char *x)
+{
+	char *y = x;
+	char z[] = {'A', 'E', 'O', 'T', 'L'};
+	int a[] = {4, 3, 0, 7, 1}
+	unsigned int b;
+
+	while (*x)
+	{
+		for (b = 0; b < sizeof(z) / sizeof(char); b++)
+		{
+			if (*x == z[b] || *c == z[b] + 32)
+			{
+				*c = 48 + a[b];
+			}
+		}
+		x++;
+	}
+
+	return (y);
+}
