08f0dd91d66e5fabf419e90af1a92864074fa61f
Mohamed <abdoashraf973@gmial.com>
2023-08-28 12:52:10 -0700

    Task Done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -16,7 +16,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (y = 0; accept[y] != str[x]; y++)
+		for (y = 0; accept[y] != s[x]; y++)
 		{
 			if (accept[y] == '\0')
 				return (x);
