ea82e7d934aebbec0a7e41dbc067accd48ba79aa
Mohamed <abdoashraf973@gmial.com>
2023-08-30 16:05:16 -0700

    Task Done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,12 +3,39 @@
 
 /**
  * is_prime_number - check if no. is prime
- * 
  * @n: int
+ *
+ * @x: int
+ *
+ * Return: 0 or 1
+*/
+
+int prime(int n, int x);
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+
+/**
+ * prime - check all no. < n if they are prime
+ *
+ * @n: int
+ *
+ * @x: int
  *
  * Return: prime
 */
 
-int is_prime_number(int n)
+int prime(int n, int x)
 {
-
+	if (x >= n && n > 1)
+	{
+		return (1);
+	}
+	else if (n % x == 0 || n <= 1)
+	{
+		return (0);
+	}
+	else
+		return (prime(n, x + 1));
+}
