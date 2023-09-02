d325439b5f5ec078d63e8e62f90559924c6dc456
Mohamed <abdoashraf973@gmial.com>
2023-09-01 18:09:14 -0700

    Task Done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ * 
+ * @argv: array
+ *
+ * Return: Always 0 succes
+*/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
