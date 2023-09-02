2a6fbaba487cbb8e679d9ce176061d1ec093c5c7
Mohamed <abdoashraf973@gmial.com>
2023-09-01 18:22:07 -0700

    Task Done

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ *
+ * @argv: pointer
+ *
+ * Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int x = 0;
+	char *y;
+
+	while (--argc)
+	{
+		for (y = argv[argc]; *y; c++)
+		{
+			if (*y < '0' || *y > '9')
+			{
+				return (printf("Error\n"), 1);
+			}
+		}
+		x += atoi(argv[argc]);
+	}
+	printf("%d\n", x);
+	return (0);
+}
