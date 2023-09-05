3e6c24915dd049e4e24aa90a77304d3cd9ca0d18
Mohamed <abdoashraf973@gmial.com>
2023-09-05 16:01:27 -0700

    Task Done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strdup -  returns a pointer to a
+ * newly allocated space
+ *
+ * @str: pointer
+ *
+ * Return: char
+*/
+
+char *_strdup(char *str)
+{
+	int x = 0;
+	int y = 0;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (; str[y] != '\0'; y++)
+	;
+
+	z = malloc(y * sizeof(*str) + 1);
+
+	if (z == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; x < y; x++)
+		{
+			z[x] = str[x];
+		}
+	}
+
+	return (z);
+}
