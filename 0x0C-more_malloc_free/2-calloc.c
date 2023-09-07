4dee03dda2e7d13fc81313c766b4194f48af1699
Mohamed <abdoashraf973@gmial.com>
2023-09-07 16:15:15 -0700

    Task Done

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -38,7 +38,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (size == 0 || nmemb == 0)
 		return (NULL);
-	x = malloc(sizeof(int) * nmemb);
+	x = malloc(size * nmemb);
 
 	if (x == 0)
 		return (NULL);
