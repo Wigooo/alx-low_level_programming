ccc8da78d309b7b9e842c42564237b20627155f4
Mohamed <abdoashraf973@gmial.com>
2023-09-13 15:43:26 -0700

    Task Done

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - index int
+ *
+ * @aray: pointer
+ *
+ * @size: int
+ *
+ * @cmp: pointer
+ *
+ * Return: int
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int x = 0;
+
+	if (array && size && cmp)
+			while (x < size)
+			{
+				if (cmp(array[x]))
+					return (x);
+				x++;
+			}
+	return (-1);
+}
