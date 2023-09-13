3ff08f393e2433d3304c8277f883e5f923d8d61d
Mohamed <abdoashraf973@gmial.com>
2023-09-13 16:16:24 -0700

    Task Done

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include "function_pointer.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: int
+ *
+ * @argv: pointer to pointer
+ *
+ * Return: int
+*/
+
+int main(int argc, char **argv)
+{
+	char *x = (char *)main;
+	int y;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	y = atoi(argv[1]);
+	if (y < 0)
+		printf("Error\n"), exit(2);
+
+	while (y--)
+	{
+		printf("%02hhx%s", *x++, y ? " " : "\n");
+	}
+	return (0);
+}
