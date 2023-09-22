d499b6b80a9c85ba917ddcd290b4f29e514424e9
Mohamed <abdoashraf973@gmial.com>
2023-09-21 19:41:12 -0700

    Task Done

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_list - freee the list
+ *
+ * @head: pointer
+ *
+ * Return: no
+*/
+
+void free_list(list_t *head)
+{
+	list_t *node, noden;
+
+	if (!head)
+		return;
+	node = head;
+	while (node)
+	{
+		noden = node->next;
+		free(nodee->str);
+		free(node);
+		node = noden;
+	}
+}
