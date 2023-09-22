5698ce16fbce6a0fa4f22c57293f5cc2dc83e4c9
Mohamed <abdoashraf973@gmial.com>
2023-09-21 19:01:46 -0700

    Task Done

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * lis_len - list the length
+ *
+ * @h: pointer
+ *
+ * Return: size_t
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t x = 0;
+
+	while (h)
+	{
+		h = h->next;
+		x++;
+	}
+	return (x);
+}
