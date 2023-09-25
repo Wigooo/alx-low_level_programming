177775dfab6d9f1f66f33ae6b5d2b78a2390a3f5
Mohamed <abdoashraf973@gmial.com>
2023-09-25 10:53:52 -0700

    Task Done

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		if (x == index)
 		{
 			nodep->next = node->next;
-			free(nodep);
+			free(node);
 			return (1);
 		}
 		x++;
