b142056c148230475f745e8f3784c838364438c0
Mohamed <abdoashraf973@gmial.com>
2023-09-25 10:45:49 -0700

    Task Done

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - same as title
+ *
+ * @head: pointer
+ * @idx: int
+ * @n: int
+ *
+ * ReturnL listint
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *n, *noden = malloc(sizeof(listint_t));
+	unsigned int i = 0;
+
+	if (!head || !noden)
+		return (NULL);
+
+	noden->n = n;
+	noden->next = NULL;
+	if (!idx)
+	{
+		noden->next = *head;
+		*head = noden;
+		return (noden);
+	}
+	node = *head;
+	while (node)
+	{
+		if (i == idx - 1)
+		{
+			noden->next = node->next;
+			node->next = noden;
+			return (noden);
+		}
+		i++;
+		node = node->next;
+	}
+	free(noden);
+	return (NULL);
+}
