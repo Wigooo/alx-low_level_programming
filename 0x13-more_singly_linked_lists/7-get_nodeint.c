a0d91facb80d708d6b615b93eb41b16dd83e9c1c
Mohamed <abdoashraf973@gmial.com>
2023-09-25 10:35:01 -0700

    Task Done

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * get_nodeeint_at_index = the same title
+ *
+ * @head: pointer
+ * @index: int
+ *
+ * ReturnL list int
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *n;
+	unsigned int x;
+
+	for (n = head, x = 0; n && x < index; n = n->next, x++)
+		;
+	return (n);
+}
