2fd141be1e228799cf609f8831b2eeb162263aee
Mohamed <abdoashraf973@gmial.com>
2023-09-25 10:27:40 -0700

    Task Done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists"
+
+/**
+ * free_listint - free the list
+ * 
+ * @head: pointer
+ *
+ * Return: no
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *n;
+
+	while (head)
+	{
+		n = head;
+		head = head->next;
+		free(n);
+	}
+}
