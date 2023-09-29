a0258008f86d63da8bad76b23d511b09202d0ee2
Mohamed <abdoashraf973@gmial.com>
2023-09-29 04:26:48 -0700

    Task Done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - binary to uint
+ *
+ * @b: char
+ *
+ * Return: int
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x = 0;
+
+	if (!b)
+	{
+		return (0);
+	}
+	while (*b)
+	{
+		if (*b != 0 && *b != '1')
+			return (0);
+		x = x * 2 + (*b++ - '0');
+	}
+	return (x);
+}
